4ff20d4064bfcc4dd52ac61bafd42894f0ea5155
gusvendegna <gusvendegna@gmail.com>
2024-10-17 19:38:37 -0600

    fixed ghost scores

--- src/App.svelte
+++ src/App.svelte
@@ -46,8 +46,8 @@
   </div>
 
 
-  <input type="text" id="date" class="bg-white text-black" bind:value={user_date}/>
-  <button type="button" on:click={handleDateChange} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+  <input type="text" id="date" class="bg-white text-black" bind:value={user_date} placeholder="YYYY-MM-DD"/>
+  <button type="button" on:click={handleDateChange} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Change Date</button>
   
   
 
@@ -90,7 +90,7 @@
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white flex">
               {game.gameDate}
-              {#if game.gameState == "LIVE"}
+              {#if game.gameState == "LIVE" || game.gameState == "CRIT"}
                 <span class="relative flex h-2 w-2 m-1.5">
                   <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
                   <span class="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
@@ -110,29 +110,29 @@
               </div>
             </td>
             <td class="px-6 py-7 flex items-center">
-              {#if game.gameState == "LIVE" || game.gameState == "OFF"}
+              {#if game.gameState != "PRE"}
                 <p class="text-md">{game.homeTeam.score}</p>
 
               {/if}
             </td>
             <td>
-              {#if game.gameState == "LIVE" || game.gameState == "OFF"}
+              {#if game.gameState != "PRE"}
                 <p class="text-md">{game.awayTeam.score}</p>
 
               {/if}
             </td>
             <td>
-              {#if game.gameState == "LIVE" || game.gameState == "OFF"}
+              {#if game.gameState != "PRE"}
                 <p class="text-md">{game.homeTeam.sog}</p>
               {/if}
             </td>
             <td>
-              {#if game.gameState == "LIVE" || game.gameState == "OFF"}
+              {#if game.gameState != "PRE"}
                 <p class="text-md">{game.awayTeam.sog}</p>
               {/if}
             </td>
             <td>
-              {#if game.gameState == "LIVE" || game.gameState == "OFF"}
+              {#if game.gameState != "PRE"}
               <div class="flex">
                 <p class="text-md">{game.period}</p>
                 {#if game.periodDescriptor.periodType == "REG"}
@@ -145,8 +145,8 @@
             {/if}
             </td>
             <td>
-              {#if game.gameState == "LIVE" || game.gameState == "OFF"}
-                <p class="text-md">{game.awayTeam.sog}</p>
+              {#if game.gameState != "PRE"}
+                <p class="text-md pl-10">n/a</p>
               {/if}
             </td>
           </tr>
